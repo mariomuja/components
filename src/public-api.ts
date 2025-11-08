194b2cf5c36d6cab130a83d70d1868f3e83fd537
mariomuja <mario.muja@gmx.de>
2025-11-08 02:48:18 +0100

    feat: Configure ng-packagr and build as npm package @mario-muja/angular-shared-components@1.0.0

---
+++ src/public-api.ts
@@ -0,0 +1,28 @@
+/*
+ * Public API Surface of @mario-muja/angular-shared-components
+ */
+
+// Login
+export * from './login';
+
+// Services  
+export * from './services';
+
+// Documentation
+export * from './documentation';
+
+// Startup
+export * from './startup';
+
+// UI Components
+export * from './ui';
+
+// Admin
+export * from './admin';
+
+// Auth
+export * from './auth';
+
+// Utils (only export email-notifier, avoid conflicts)
+export { EmailNotifier } from './utils/email-notifier';
+
